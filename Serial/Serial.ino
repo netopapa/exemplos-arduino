82c17f0d0851750253c79c45d69376aaa0fdf0d8
Papa Neto <papaneto@hotmail.com>
2017-05-29 22:48:47 -0400

    Subindo teste da lib Serial

---
+++ Serial/Serial.ino
@@ -0,0 +1,18 @@
+const int pin = 13;
+char tecla;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tecla = Serial.read();
+  if(tecla == 'l'){
+    digitalWrite(pin, true);
+  }else if(tecla == 'd'){
+    digitalWrite(pin, false);
+  }
+}
